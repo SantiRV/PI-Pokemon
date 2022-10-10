5e2b7bf5589475c17ad980b59db9c937ff590185
Santiago Rambeaud <santy_r_7@hotmail.com>
2022-10-09 21:43:27 -0300

    c2

---
+++ client/src/actions/index.js
@@ -0,0 +1,135 @@
+import axios from 'axios';
+import {
+    GET_POKEMON,
+    GET_TYPES,
+    CREATED_DB,
+    FILTER_ATTACK,
+    FILTER_ALPHABETICAL,
+    FILTER_BY_TYPE,
+    GET_POKEMON_DETAIL,
+    RESET_DETAIL,
+    GET_POKEMON_NAME
+} from './constants';
+
+export function getPokemon() {
+    return async (dispatch) => {
+        try {
+            let pokemon = await axios('http://localhost:3001/pokemons');
+            return dispatch({
+                type: GET_POKEMON,
+                payload: pokemon.data
+            })
+        } catch(error) {
+            console.log(error);
+        }
+    }
+};
+
+export function getPokemonTypes() {
+    return async (dispatch) => {
+        try {
+            let pokemonTypes = await axios('http://localhost:3001/types');
+            console.log(pokemonTypes)
+            return dispatch({
+                type: GET_TYPES,
+                payload: pokemonTypes.data
+            })
+        } catch(error) {
+            console.log(error);
+        }
+    }
+};
+
+export function filterPokemonsByType(payload) {
+    console.log(payload);
+    return {
+        type: FILTER_BY_TYPE,
+        payload
+    }
+};
+
+export function filterCreatedDB(payload) {
+    console.log(payload);
+    return {
+        type: CREATED_DB,
+        payload
+    }
+};
+
+export function filterAttack(payload) {
+    console.log(payload);
+    return {
+        type: FILTER_ATTACK,
+        payload
+    }
+};
+
+export function filterAlphabetical(payload) {
+    return {
+        type: FILTER_ALPHABETICAL,
+        payload
+    }
+};
+
+export function getPokemonDetail(id) {
+    console.log(id, 'pokeId');
+    return async function(dispatch) {
+        try {
+            let json = await axios.get(`http://localhost:3001/pokemons/${id}`);
+            console.log(json, 'Detalle del poke');
+            return dispatch({
+                type: GET_POKEMON_DETAIL,
+                payload: json.data
+            })
+        } catch(error) {
+            console.log(error);
+        }
+    }
+};
+
+export function getPokeName(name) {
+    return async function(dispatch) {
+        try {
+            const pokeName = await axios.get(`http://localhost:3001/pokemons?name=${name}`);
+            console.log(pokeName.data.name);
+            return dispatch({
+                type: GET_POKEMON_NAME,
+                payload: pokeName.data
+            })
+        } catch(error) {
+            alert('Debes ingresar el nombre completo!')
+        }
+    }
+};
+
+export function getClonePoke(name) {
+    return async function () {
+        const pokeName = await axios.get(`http://localhost:3001/pokemons?name=${name}`);
+        if (!pokeName) {
+            return false
+        }
+        return true
+    }
+};
+
+export function createPokemon(details) {
+    return async function (dispatch) {
+        try {
+            const newPokemon = await axios.post('http://localhost:3001/pokemons', details)
+            return dispatch({
+                type: 'CREATE_POKEMON',
+                payload: newPokemon.data
+            })
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+};
+
+export function resetDetail(payload) {
+    return {
+        type: RESET_DETAIL,
+        payload
+    }
+};
---
+++ client/src/reducers/index.js
@@ -0,0 +1,113 @@
+const initialState = {
+    pokemons: [],
+    allPokemons: [],
+    types: [],
+    detail: []
+};
+
+function rootReducer(state = initialState, action) {
+    switch(action.type) {
+        case 'GET_POKEMON':
+            console.log(state.allPokemons);
+            return {
+                ...state,
+                pokemons: action.payload,
+                allPokemons: action.payload
+            }
+        case 'FILTER_BY_TYPE':
+            const allPokes = state.allPokemons;
+            const typeFiltered = action.payload === 'todos' ? allPokes :
+            allPokes.filter(pt => pt.types.map(pt => pt.name).includes(action.payload))
+            console.log(typeFiltered);
+            return {
+                ...state,
+                pokemons: typeFiltered
+            }
+        case 'GET_TYPES':
+            return {
+                ...state,
+                types: action.payload
+            }
+        case 'CREATED_DB':
+            const pokemonAll = state.allPokemons;
+            const cratedInDb = action.payload === 'creados' ?
+            pokemonAll.filter(pokemon => pokemon.cratedInDb) :
+            pokemonAll.filter(pokemon => !pokemon.cratedInDb)
+            console.log('Creados en DB', cratedInDb.length);
+            return {
+                ...state,
+                pokemons: action.payload === 'todos' ? pokemonAll : cratedInDb
+            }
+        case 'FILTER_ATTACK':
+            let pokemonByAttack = action.payload === 'mayor' ?
+            state.pokemons.sort(function(a, b) {
+                if(a.attack > b.attack) {
+                    return -1
+                }
+                if(b.attack > a.attack) {
+                    return 1
+                }
+                return 0
+            }) :
+            state.pokemons.sort(function(a, b) {
+                if(a.attack > b.attack) {
+                    return 1
+                }
+                if(b.attack > a.attack) {
+                    return -1
+                }
+                return 0
+            })
+        return {
+            ...state,
+            pokemons: pokemonByAttack
+        }
+        case 'FILTER_ALPHABETICAL':
+            let orderByName = action.payload === 'a-z' ?
+            state.pokemons.sort(function (a, b) {
+                if (a.name > b.name) {
+                    return 1;
+                }
+                if (b.name > a.name) {
+                    return -1
+                }
+                return 0
+            }) :
+            state.pokemons.sort(function (a, b) {
+                if (a.name > b.name) {
+                    return -1;
+                }
+                if (b.name > a.name) {
+                    return 1
+                }
+                return 0
+            })
+        return {
+            ...state,
+            pokemons: orderByName
+        }
+        case 'GET_POKEMON_DETAIL':
+            return {
+                ...state,
+                detail: action.payload
+            }
+        case 'RESET_DETAIL':
+            return {
+                ...state,
+                detail: []
+            }
+        case 'GET_POKEMON_NAME':
+            return {
+                ...state,
+                pokemons: action.payload
+            }
+        case 'CREATE_POKEMON':
+            return{
+                ...state,
+            }
+        default:
+            return state;
+    }
+};
+
+export default rootReducer;
